75effd10102e3f6173c9a3879b03549314439b3f
Sifeddine <sifeddinebaouz@gmail.com>
2025-02-02 16:57:55 +0100

    fix: fix the files

--- number_guess.sh
+++ number_guess.sh
@@ -3,41 +3,64 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
 COUNTER=1
+
 NUMBER=$(( RANDOM % 1001 ))
 
 echo "Enter your username:"
 read USERNAME
+
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME';")
+
 if [[ -z $USER_ID ]] ; then
+
     echo "Welcome, $USERNAME! It looks like this is your first time here."
-    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+
+    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
   else
-    USER_INFO=$($PSQL "SELECT COUNT(*) AS games_played, MIN(tries) AS best_game FROM games WHERE user_id=$USER_ID")
+
+    USER_INFO=$($PSQL "SELECT COUNT(*) AS games_played, MIN(tries) AS best_game FROM games WHERE user_id=$USER_ID;")
+
     echo "$USER_INFO" | while read GAMES_PLAYED BAR BEST_GAME
+
     do
+
       echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+
     done
 fi
+
 echo "Guess the secret number between 1 and 1000:"
 read INPUT_NUMBER
 
 while [[ ! $INPUT_NUMBER =~ ^[0-9]+$ ]] ; do
+
   echo "That is not an integer, guess again:"
   read INPUT_NUMBER
+
 done
 
 while [[ $INPUT_NUMBER != $NUMBER ]] ; do
+
   if [[ $INPUT_NUMBER -gt $NUMBER ]] ; then
+
     COUNTER=$(( COUNTER + 1 ))
+
     echo "It's lower than that, guess again:"
     read INPUT_NUMBER
+
   elif [[ $INPUT_NUMBER -lt $NUMBER ]] ; then
+
     COUNTER=$(( COUNTER + 1 ))
+
     echo "It's higher than that, guess again:"
     read INPUT_NUMBER
+
   fi
 done
+
 INSER_GAME_INFO=$($PSQL "INSERT INTO games(user_id, tries) VALUES($USER_ID, $COUNTER);")
+
 echo "You guessed it in $COUNTER tries. The secret number was $NUMBER. Nice job!"
 
--- number_guess.sql
+++ number_guess.sql
@@ -130,47 +130,100 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.games VALUES (10, 12, 20);
-INSERT INTO public.games VALUES (11, 12, 7);
-INSERT INTO public.games VALUES (12, 13, 251);
-INSERT INTO public.games VALUES (13, 13, 66);
-INSERT INTO public.games VALUES (14, 14, 478);
-INSERT INTO public.games VALUES (15, 14, 853);
-INSERT INTO public.games VALUES (16, 13, 117);
-INSERT INTO public.games VALUES (17, 13, 400);
-INSERT INTO public.games VALUES (18, 13, 870);
-INSERT INTO public.games VALUES (19, 15, 584);
-INSERT INTO public.games VALUES (20, 15, 822);
-INSERT INTO public.games VALUES (21, 16, 14);
-INSERT INTO public.games VALUES (22, 16, 234);
-INSERT INTO public.games VALUES (23, 15, 504);
-INSERT INTO public.games VALUES (24, 15, 1001);
-INSERT INTO public.games VALUES (25, 15, 363);
+INSERT INTO public.games VALUES (26, 17, 525);
+INSERT INTO public.games VALUES (27, 17, 909);
+INSERT INTO public.games VALUES (28, 18, 285);
+INSERT INTO public.games VALUES (29, 18, 640);
+INSERT INTO public.games VALUES (30, 17, 188);
+INSERT INTO public.games VALUES (31, 17, 724);
+INSERT INTO public.games VALUES (32, 17, 992);
+INSERT INTO public.games VALUES (33, 19, 466);
+INSERT INTO public.games VALUES (34, 19, 771);
+INSERT INTO public.games VALUES (35, 20, 936);
+INSERT INTO public.games VALUES (36, 20, 38);
+INSERT INTO public.games VALUES (37, 19, 680);
+INSERT INTO public.games VALUES (38, 19, 389);
+INSERT INTO public.games VALUES (39, 19, 115);
+INSERT INTO public.games VALUES (40, 21, 735);
+INSERT INTO public.games VALUES (41, 21, 927);
+INSERT INTO public.games VALUES (42, 22, 802);
+INSERT INTO public.games VALUES (43, 22, 400);
+INSERT INTO public.games VALUES (44, 21, 155);
+INSERT INTO public.games VALUES (45, 21, 410);
+INSERT INTO public.games VALUES (46, 21, 516);
+INSERT INTO public.games VALUES (47, 23, 456);
+INSERT INTO public.games VALUES (48, 23, 285);
+INSERT INTO public.games VALUES (49, 24, 890);
+INSERT INTO public.games VALUES (50, 24, 298);
+INSERT INTO public.games VALUES (51, 23, 221);
+INSERT INTO public.games VALUES (52, 23, 394);
+INSERT INTO public.games VALUES (53, 23, 433);
+INSERT INTO public.games VALUES (54, 25, 580);
+INSERT INTO public.games VALUES (55, 25, 944);
+INSERT INTO public.games VALUES (56, 26, 857);
+INSERT INTO public.games VALUES (57, 26, 142);
+INSERT INTO public.games VALUES (58, 25, 997);
+INSERT INTO public.games VALUES (59, 25, 900);
+INSERT INTO public.games VALUES (60, 25, 204);
+INSERT INTO public.games VALUES (61, 27, 423);
+INSERT INTO public.games VALUES (62, 27, 853);
+INSERT INTO public.games VALUES (63, 28, 466);
+INSERT INTO public.games VALUES (64, 28, 752);
+INSERT INTO public.games VALUES (65, 27, 416);
+INSERT INTO public.games VALUES (66, 27, 732);
+INSERT INTO public.games VALUES (67, 27, 667);
+INSERT INTO public.games VALUES (68, 29, 10);
+INSERT INTO public.games VALUES (69, 30, 112);
+INSERT INTO public.games VALUES (70, 30, 851);
+INSERT INTO public.games VALUES (71, 31, 879);
+INSERT INTO public.games VALUES (72, 31, 570);
+INSERT INTO public.games VALUES (73, 30, 751);
+INSERT INTO public.games VALUES (74, 30, 634);
+INSERT INTO public.games VALUES (75, 30, 380);
+INSERT INTO public.games VALUES (76, 32, 244);
+INSERT INTO public.games VALUES (77, 32, 405);
+INSERT INTO public.games VALUES (78, 33, 275);
+INSERT INTO public.games VALUES (79, 33, 224);
+INSERT INTO public.games VALUES (80, 32, 568);
+INSERT INTO public.games VALUES (81, 32, 800);
+INSERT INTO public.games VALUES (82, 32, 118);
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.users VALUES (12, 'Sif');
-INSERT INTO public.users VALUES (13, 'user_1738510010597');
-INSERT INTO public.users VALUES (14, 'user_1738510010596');
-INSERT INTO public.users VALUES (15, 'user_1738510093882');
-INSERT INTO public.users VALUES (16, 'user_1738510093881');
+INSERT INTO public.users VALUES (17, 'user_1738510844091');
+INSERT INTO public.users VALUES (18, 'user_1738510844090');
+INSERT INTO public.users VALUES (19, 'user_1738510855133');
+INSERT INTO public.users VALUES (20, 'user_1738510855132');
+INSERT INTO public.users VALUES (21, 'user_1738510958953');
+INSERT INTO public.users VALUES (22, 'user_1738510958952');
+INSERT INTO public.users VALUES (23, 'user_1738511331431');
+INSERT INTO public.users VALUES (24, 'user_1738511331430');
+INSERT INTO public.users VALUES (25, 'user_1738511345084');
+INSERT INTO public.users VALUES (26, 'user_1738511345083');
+INSERT INTO public.users VALUES (27, 'user_1738511411178');
+INSERT INTO public.users VALUES (28, 'user_1738511411177');
+INSERT INTO public.users VALUES (29, 'Sif');
+INSERT INTO public.users VALUES (30, 'user_1738511622452');
+INSERT INTO public.users VALUES (31, 'user_1738511622451');
+INSERT INTO public.users VALUES (32, 'user_1738511663836');
+INSERT INTO public.users VALUES (33, 'user_1738511663835');
 
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.games_game_id_seq', 25, true);
+SELECT pg_catalog.setval('public.games_game_id_seq', 82, true);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 16, true);
+SELECT pg_catalog.setval('public.users_user_id_seq', 33, true);
 
 
 --
